c742de82eba0cef787bed9ee988b9f880be7c6e7
Arnav-Gr0ver <arnavgrover@gmail.com>
2025-03-02 07:55:31 +0000

    test
    Signed-off-by: Arnav-Gr0ver <arnavgrover@gmail.com>

--- train/sbatch.sh
+++ train/sbatch.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 #SBATCH --cpus-per-task=8
-#SBATCH --mem=24G
+#SBATCH --mem=20G
 #SBATCH --account=csso-e
 #SBATCH --gres=gpu:1
 #SBATCH --time=4:00:00
@@ -9,9 +9,9 @@
 module load cuda/12.1.1 cudnn/cuda-12.1_8.9 anaconda
 conda activate /scratch/gilbreth/jsetpal/conda/workshop
 
-cd ~/git/p1
+cd ~/arnav-temp/p1
 
 MLFLOW_TRACKING_USERNAME=$MLFLOW_USERNAME \
 MLFLOW_TRACKING_PASSWORD=$MLFLOW_TOKEN \
-OMP_NUM_THREADS=8 \
-python finetune.py
+OMP_NUM_THREADS=80 \
+torchrun --standalone --nnodes=1 --nproc_per_node=gpu -m train.finetune
